b13d108f6c141477104947e81f239801ecadee81
wowenemy <35076761+wowenemy@users.noreply.github.com>
2020-12-24 22:10:56 +0200

    Resize (#4)
    * first modification
    
    * start resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,20 @@ import { createTable } from './table.template';
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
-  toHTML() {
-    return createTable(100);
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    })
   }
+
+  toHTML() {
+    return createTable(150);
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      console.log('start');
+    }
+  }
+
 }
